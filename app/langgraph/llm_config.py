2719d52c17737664d39cf99ffe1c8385ec530950
Vedant Kotkar <vedantkotkar111@gmail.com>
2025-05-03 20:28:31 +0530

    removed workflow

--- app/langgraph/llm_config.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-from app.core.config import OPENAI_API_KEY
-import sqlite3
-from langgraph.checkpoint.sqlite import SqliteSaver
-conn = sqlite3.connect('checkpoints.sqlite3',check_same_thread=False)
-
-
-memory = SqliteSaver(conn)   
-llm = ChatOpenAI(model="gpt-4o",api_key=OPENAI_API_KEY)
